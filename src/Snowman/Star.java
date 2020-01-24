4687827ec94fa33629cb008f9e547ba17c81cdc3
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2020-01-24 18:10:40 +0200

    Star drawing

--- src/Snowman/Star.java
+++ src/Snowman/Star.java
@@ -4,7 +4,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
 public class Star extends BorderPane {
@@ -20,9 +20,9 @@ Star () {
         IntegerConvertor integerConvertor = new IntegerConvertor();
         this.radius = integerConvertor.convertTextToInteger(primaryStage, Radius);
 
-        if (this.radius == 0) {       //  Проверка на то, заполнено ли поле для ввода.
-            return;                   //  Значение радиуса обязательно должно быть целым числом
-        }                             //  Отрицательное число будет преобразовано в положительное
+        if (this.radius == 0) {       //  Проверка на то, заполнены ли поля для ввода.
+            return;                   //  Значения обязательно должны быть целыми числами.
+        }                             //  Отрицательные числа будет преобразованы в положительные.
 
         System.out.println("\nStar !");
 
@@ -33,44 +33,109 @@ Star () {
 
         primaryStage.setTitle(" Star ");
 
-        Scene scene = new Scene(group,600,500);
+        Scene scene = new Scene(group,800,600);
         primaryStage.setScene(scene);
 
-
-    /*  Тестовый код
-
-        Circle circle = new Circle();
-        circle.setCenterX(250);
-        circle.setCenterY(150);
-        circle.setRadius(100);
-
-        circle.setStroke(Color.color(Snowman.randomColor()[0], Snowman.randomColor()[1], Snowman.randomColor()[2]));
-        circle.setFill(Color.WHITE);
-
-        group.getChildren().addAll(circle);
-
-        Circle circle2 = new Circle();
-        circle2.setCenterX(250);
-        circle2.setCenterY(150);
-        circle2.setRadius(50);
-
-        circle2.setStroke(Color.color(Snowman.randomColor()[0], Snowman.randomColor()[1], Snowman.randomColor()[2]));
-        circle2.setFill(Color.RED);
-
-        group.getChildren().addAll(circle2);
-
-   // */
+     //
+     //   ((Group) scene.getRoot()).getChildren().add(root);    //  Добавить в группу
+     //
 
 
+
+        Path path = new Path();
+
+ /*
+        MoveTo moveTo = new MoveTo(108, 71);
+
+        LineTo line1 = new LineTo(321, 161);
+        LineTo line2 = new LineTo(126,232);
+        LineTo line3 = new LineTo(232,52);
+        LineTo line4 = new LineTo(269, 250);
+        LineTo line5 = new LineTo(108, 71);
+
+
+        path.getElements().add(moveTo);
+        path.getElements().addAll(line1, line2, line3, line4, line5);
+
+
+        path.setStroke(Color.BLUE);
+        path.setStrokeWidth(5);
+
+        group.getChildren().add(path);
+
+  */
+
+        double centerX;
+        double centerY;
+        double innerRadius;
+        double outerRadius;
+        int numRays;
+        double startAngleR;    //  Угол поворота звезды
+
+        centerX = 300;
+        centerY = 250;
+        innerRadius = 50;
+        outerRadius = 100;
+        numRays = 12;
+        startAngleR = 0;
+
+        path = (Path) drawStar(centerX, centerY, innerRadius, outerRadius, numRays, startAngleR);
+
+
+        path.setStroke(Color.BLUE);
+        path.setStrokeWidth(5);
+
+        group.getChildren().add(path);
 
 
 
 
         primaryStage.show();
 
-
     }
 
+
+
+    public Shape drawStar(double x, double y,
+                                  double innerRadius, double outerRadius, int numRays,
+                                  double startAngleR)
+    {
+        double deltaAngleR = Math.PI / numRays;
+        Path path = new Path();
+
+        for (int i = 0; i < numRays * 2 + 1; i++)
+        {
+            double angleR = startAngleR + i * deltaAngleR;
+            double ca = Math.cos(angleR);
+            double sa = Math.sin(angleR);
+            double relX = ca;
+            double relY = sa;
+            if ((i & 1) == 0)
+            {
+                relX *= outerRadius;
+                relY *= outerRadius;
+            }
+            else
+            {
+                relX *= innerRadius;
+                relY *= innerRadius;
+            }
+            if (i == 0)
+            {
+                MoveTo moveTo = new MoveTo(x + relX, y + relY);
+                path.getElements().add(moveTo);
+
+            }
+            else
+            {
+                LineTo line = new LineTo(x + relX, y + relY);
+                path.getElements().add(line);
+
+            }
+        }
+
+        return path;
+    }
 
 
 }
